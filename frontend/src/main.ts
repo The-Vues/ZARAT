b3698b810fe035ac5372a7e34430928e2fd2cf4f
RayenTellissy <rayentellissy571@gmail.com>
2023-05-23 19:14:52 +0100

    added vue routing

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,18 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const routes = [
+  { path: "/", component: /* home component here*/},
+  { path: "/login", component: /* login component here*/ },
+  { path: "/signup", component: /* sign up component here */},
+  { path: "/cart", component: /* cart component here */}
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+createApp(App).use(router).mount('#app')
