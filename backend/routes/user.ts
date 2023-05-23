0aa9961ea78a04d1cd076e62eda0b643d2265190
RayenTellissy <rayentellissy571@gmail.com>
2023-05-23 16:10:29 +0100

    .

--- backend/routes/user.ts
+++ backend/routes/user.ts
@@ -3,12 +3,12 @@ import User from '../controller/user';
 
 const router: Router = express.Router();
 
-router.get('/getAll', User.getAll);
-router.get("/refresh/:id", User.refreshUser)
+// router.get('/getAll', User.getAll);
+// router.get("/refresh/:id", User.refreshUser)
 
-router.post("/login",User.login)
-router.post("/signup", User.signup)
-router.post('/add', User.addToCart);
+// router.post("/login",User.login)
+// router.post("/signup", User.signup)
+// router.post('/add', User.addToCart);
 
 
 export default router;
