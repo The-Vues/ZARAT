f3d2236866f3e8ddddd9b623ff63a1e94e69c2e2
RayenTellissy <rayentellissy571@gmail.com>
2023-05-23 21:19:44 +0100

    search fully functional in the backend

--- backend/controller/items.ts
+++ backend/controller/items.ts
@@ -9,13 +9,17 @@ interface SearchRequest extends Request{
 
 export default {
 
+  // method to get all items from the database
   getAll: (req: Request, res: Response) => {
     items.find().then(result => res.send(result))
   },
   
+  // method used to filter items by gender and query at the same, (used in the search component in the frontend)
   searchByGender: (req: SearchRequest, res: Response) => {
+
     const { query } = req.query
     const { gender } = req.params
+
     items.find({
       $and: [
         { keyword: {$regex: new RegExp(query, "i")} },
--- backend/routes/items.ts
+++ backend/routes/items.ts
@@ -4,6 +4,7 @@ import item  from '../controller/items';
 const router: Router = express.Router();
 
 router.get("/getAll", item.getAll)
+router.get("/search/:gender", item.searchByGender)
 // router.get("/gender/:gender", item.getByGender)
 
 // router.get("/cartItems/:id", item.getCartItems)
