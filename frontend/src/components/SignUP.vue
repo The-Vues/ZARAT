3536fa915f9d5a5fd0862cea19d83f0fdc260956
Mehdi Dissem <amriahmed1990@gmail.com>
2023-05-26 12:03:16 +0100

    edit front signUp

--- frontend/src/components/SignUP.vue
+++ frontend/src/components/SignUP.vue
@@ -1,234 +1,214 @@
 <template>
   <div>
-    <Navbar/>
-    <nav className="">
-        <div className="flex justify-between items-center p-5">
-          <div className="grid gap-1 grid-cols-2">
-            <div>
-              <Link href="/">
-                  <svg viewBox="0 0 100 80" width="25" height="25">
-                    <rect width="70" height="5"></rect>
-                    <rect y="30" width="70" height="5"></rect>
-                    <rect y="60" width="70" height="5"></rect>
-                  </svg>
-              </Link>
-            </div>
+    <Navbar />
+    <div class="container">
+      <div class="left-form">
+        <h2 class="heading-form">PERSONAL DETAILS</h2>
+        <div class="input-text-box">
+          <input class="input-text" type="text" id="email" name="email" placeholder="E-MAIL" v-model="email" />
+        </div>
+        <div class="input-text-box">
+          <div class="input-inline">
+            <input class="input-text" type="password" id="password" name="password" placeholder="PASSWORD" v-model="password" />
+            <input class="input-text" type="password" id="repeatPassword" name="repeatPassword" placeholder="REPEAT PASSWORD" v-model="repeatPassword" />
           </div>
         </div>
-      </nav>
-   <h2 className='headingform'>PERSONAL DETAILS</h2>
-   <div className='changeform-btn'>
-   </div>
-  
-<div className='container'>
-  <div className='Left-form'> 
-  <div className='input-text-box'>
-      <input className="input-text" type="text" id="fname" name="email" placeholder='E-MAIL'   />
-  </div>
-  <div className='input-text-box'>
-      <input   type="password" id="fname" name="password" placeholder='PASSWORD'   />
-  </div>
-  <div className='input-text-box1'>
-   </div>
-  <div className='checkbox'>
-    <div>
-      <input className="form-input-checkbox__input" type="checkbox" name="newsletterCheck" data-qa-input-qualifier="newsletterCheck" />
+        <div class="input-text-box">
+          <div class="input-inline">
+            <input class="input-text" type="text" id="name" name="name" placeholder="NAME" v-model="name" />
+            <input class="input-text" type="text" id="surname" name="surname" placeholder="SURNAME" v-model="surname" />
+          </div>
+        </div>
+        <div class="checkbox">
+          <div>
+            <input class="form-input-checkbox__input" type="checkbox" name="newsletterCheck" v-model="newsletterCheck" />
+          </div>
+          <div>
+            <h6 class="checkbox__label">I want to receive personalized commercial communications from ZARA by email.</h6>
+          </div>
+        </div>
+        <div class="checkbox">
+          <div>
+            <input class="form-input-checkbox__input" type="checkbox" name="privacyCheck" v-model="privacyCheck" />
+          </div>
+          <div>
+            <h6 class="checkbox__label">I have read and understand the Privacy and Cookies Policy.</h6>
+          </div>
+        </div>
+        <button class="signupbtn" @click="handleSubmit">CREATE ACCOUNT</button>
+      </div>
     </div>
-  <div>
-  <h6 className="checkbox__label">I want to receive personalised commercial communications from ZARA by email.</h6>
   </div>
-  </div>
+</template>
 
-  <div className='checkbox'>
-    <div>
-      <input className="form-input-checkbox__input" type="checkbox" name="newsletterCheck" data-qa-input-qualifier="newsletterCheck" />
-    </div>
-  <div>
-  <h6 className="checkbox__label">I have read and understand the Privacy and Cookies Policy</h6>
-  </div>
-  </div>
-  <input type="submit" className='signupbtn' value="CREATE ACCOUNT" @Click="handleSubmit"/>
-  </div> 
- 
- <div className='right-form'>
-<div className='input-text-box'>
-      <h1 className='dot'>.</h1>
-  </div>
-<div className='input-text-box2'>
-      <input type="password" id="fname" name="fname" placeholder='REPEATE PASSWORD'/>
-  </div>
 
-  <div className='input-text-box'>
-      <input type="text" id="fname" name="fname" placeholder='NAME'  />
-  </div>
-  <div className='input-text-box'>
-      <input type="text" id="fname" name="fname" placeholder='SURNAME'/>
-  </div>
-  <div className='input-text-box'>
-  </div>
-</div> 
-</div>
-</div>
-  </template>
-  
-  <script lang="ts">
-  import { defineComponent, ref } from 'vue';
-  import axios from 'axios';
-  
-  import Navbar from './Navbar.vue';
-  
-  export default defineComponent({
-    name: 'SignUP',
-    components: {
-      Navbar,
-    },
-    /* this code is to create a new user and send it to the backend with some constrains if the pass or firstname or lastname
-   or email are empty they can't create a new account and the pass at least must be 8 char */
-    setup() {
-      const email = ref('');
-      const password = ref('');
-      const fName = ref('');
-      const lName = ref('');
-  
-      const handleSubmit = () => {
+<script>
+import { defineComponent, ref } from 'vue';
+import axios from 'axios';
+
+import Navbar from './Navbar.vue';
+
+export default defineComponent({
+  name: 'Signup',
+  components: {
+    Navbar,
+  },
+  /* this code is to create a new user and send it to the backend with some constraints if the pass or firstname or lastname
+   or email are empty they can't create a new account, and the password must be at least 8 characters */
+  setup() {
+    const email = ref('');
+    const password = ref('');
+    const name = ref('');
+    const surname = ref('');
+
+    const handleSubmit = () => {
+      if (
+        email.value.length === 0 ||
+        password.value.length === 0 ||
+        name.value.length === 0 ||
+        surname.value.length === 0
+      ) {
+        return alert('Please fill in all the fields.');
+      } else if (password.value.length < 8) {
+        return alert('Password is weak. It must be at least 8 characters long.');
+      } else {
         const logUser = {
           email: email.value,
           pass: password.value,
-          fName: fName.value,
-          lName: lName.value,
+          fName: name.value,
+          lName: surname.value,
           isAdmin: false,
           cart: [],
         };
 
-   if((email.value as any).length ===0 || (fName.value as any).length===0 || (password.value as any).length===0 || (lName.value as any).length===0){
-      return alert('Please fill all the fields');
-    }
-    else if((password.value as any ).length < 6 ){
-      return alert('Password is weak try to make it strong at least it must be 8 characters')
-    }
-        else{
-          axios.post('http://localhost:3001/user/signup',logUser)
-            .then((user) => {
-              
-              if (user.data === 'Email Already Exists') {
-                 return alert('Email Already Exists');
-              } else {
-               return alert('Welcome to ZARA');
-              }
-            });
-        }
-      };
-  
-      return {
-        email,
-        password,
-        fName,
-        lName,
-        handleSubmit,
-      };
-    },
-  });
-  </script>
-  
+        axios.post('http://localhost:3001/user/signup', logUser)
+          .then((response) => {
+            if (response.data === 'Email Already Exists') {
+              alert('Email Already Exists');
+            } else {
+              alert('Welcome to ZARA');
+            }
+          })
+          .catch((error) => {
+            console.error(error);
+            alert('An error occurred while creating the account.');
+          });
+      }
+    };
 
-<style>
-.container{
-    width: 100%;
-    /* border: 1px solid red; */
-    display: flex;
-    margin: 4%;
-  }
-  input{
-    width: 80%;
-    padding:5px 12px 0 20px;
-    margin: 18px 0;
-    box-sizing: border-box;
-    border: none;
-    border-bottom: 1px solid gray;
-    font-size:70%;
-   
-    
-  }
-  .number-box{
-    display: flex;
-  }
-  .input-text-box1{
-    display: flex;
-    
-  }
-  .tele{
-    width: 25%;
-    margin-top: 9%;
-  }
-  .teleinput{
-    margin-left:-10%;
-    width:96%;
-  }
-  .checkbox{
-    width: 100%;
-    display: flex;
-    margin-top: 1%;
-   
-  }
-  .checkbox__label{
-    width: 100%;
-    margin-left: 3%;
-    font-size: 100%;
-    color:  gray;
-    font-size:70%
-  
-  }
-  .signupbtn{
-  width: 85%;
-  background-color: black;
-  color: white;
-  margin-top: 16%;
-  
-  }
-  .Left-form{
-    /* border: 1px solid red; */
-    width: 30%;
-  }
-  .right-form{
-    /* border: 1px solid green; */
-    width: 40%;
-  }
-  .dot{
-    color: white
-    ;
-  }
-  .input-text-options{
-  font-size: 90%;
-  color: gray;
-  }
-  .changeform-btn{
+    return {
+      email,
+      password,
+      name,
+      surname,
+      handleSubmit,
+    };
+  },
+});
+</script>
+
+<style scoped>
+.container {
   display: flex;
-  width: 14%;
-  justify-content: space-between;
-  margin: 4%;
-  font-size: 80%;
-  font-weight:bold;
-  }
-  .headingform{
-    margin: 4%;
-    font-size: 98%;
-    color: black;
-  }
-  .signup-footer{
-    display: flex;
-    width: 40%;
-    /* border: 1px solid black; */
-    justify-content: space-between;
-    margin: 8%;
-  }
-  .list{
-  text-decoration: none;
-  color:gray;
-  font-size: 68%;
-  }
-  .header-heading{
-    font-size: 80%;
-  }
+  margin: 6% 6%;
+}
+
+.left-form {
+  width: 100%;
+  max-width: 400px;
+}
+
+.heading-form {
+  font-size: 14px;
+  margin-top: 0;
+  color: #000;
+  font-weight: 550;
+  margin-bottom: 20px;
+}
+
+.input-text-box {
+  margin-bottom: 20px;
+}
+
+.input-text {
+  width: 100%;
+  border: none;
+  border-bottom: 1px solid #c4c4c4;
+  font-size: 14px;
+  background-color: transparent;
+  color: #000;
+}
+
+.input-text:focus{
+outline: none;
+}
+.input-inline {
+  display: flex;
+  margin-bottom: 20px;
+}
+
+.input-inline .input-text {
+  flex: 1;
+  margin-right: 10px;
+}
+
+.checkbox {
+  display: flex;
+  align-items: center;
+  margin-bottom: 15px;
+  font-size: 14px;
+  color: #000;
+}
+
+.checkbox__label {
+  font-size: 14px;
+  color: #000;
+  margin-left: 10px;
+}
+
+.signupbtn {
+  width: 100%;
+  background-color: white;
+  color: black;
+  padding: 14px;
+  font-size: 14px;
+  margin-top: 20px;
+  border: 2px solid black; /* Add border */
+  cursor: pointer;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: 700;
+}
+
+.signupbtn:hover {
+  background-color: #222;
+}
+
+.signupbtn:active {
+  background-color: #111;
+}
+
+input[type='password']::-webkit-input-placeholder,
+input[type='text']::-webkit-input-placeholder {
+  font-size: 14px;
+  color: #6a6a6a;
+}
+
+input[type='password']:-moz-placeholder,
+input[type='text']:-moz-placeholder {
+  font-size: 14px;
+  color: #6a6a6a;
+}
+
+input[type='password']::-moz-placeholder,
+input[type='text']::-moz-placeholder {
+  font-size: 14px;
+  color: #6a6a6a;
+}
+
+input[type='password']:-ms-input-placeholder,
+input[type='text']:-ms-input-placeholder {
+  font-size: 14px;
+  color: #6a6a6a;
+}
 </style>
-  
-  
